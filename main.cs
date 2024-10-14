41dc6f2e5c0c6da3e02a778acfe0fb5bc2fda896
BozhidarBoyadzhiev <bpboyadzhiev20@codingburgas.bg>
2024-10-14 11:17:16 +0300

    Convert c++ code to c#

---
+++ main.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 16, 17, 15, 0, 20, 1, 1, 2 };
+        int n = arr.Length;
+        int maxLoad = 0, load = 0, diff = 0;
+
+        load = arr.Sum();
+
+        if (load % n != 0)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        load /= n;
+
+        for (int i = 0; i < n; i++)
+        {
+            diff += (arr[i] - load);
+            maxLoad = Math.Max(maxLoad, Math.Abs(diff));
+        }
+
+        Console.WriteLine(maxLoad);
+    }
+}
